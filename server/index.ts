92f56ca7f3903551114fd0c97c71c51a8e2d23fb
droooney <d.vor7@mail.ru>
2024-01-08 04:42:26 +0500

    set up server

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,42 @@
-import http from 'node:http';
+import net from 'node:net';
 
-import { blue, green } from 'colors/safe';
+import { blue, green, red } from 'colors/safe';
 
 const PORT = Number(process.env.PORT);
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+const server = net.createServer((socket) => {
+  socket.once('data', (data) => {
+    const dataString = data.toString();
+    const isHttps = data.includes('CONNECT');
+    let serverPort;
+    let serverAddress;
 
-  res.end('');
+    if (isHttps) {
+      serverPort = 443;
+      serverAddress = dataString.split('CONNECT')[1].split(' ')[1].split(':')[0];
+    } else {
+      serverPort = 80;
+      serverAddress = dataString.split('Host: ')[1].split('\n')[0].trim();
+    }
+
+    const proxySocket = net.createConnection({
+      host: serverAddress,
+      port: serverPort,
+    });
+
+    proxySocket.on('error', (err) => {
+      console.log(red(err.stack ?? err.message));
+    });
+
+    if (isHttps) {
+      socket.write('HTTP/1.1 200 OK\n\n');
+    } else {
+      proxySocket.write(data);
+    }
+
+    socket.pipe(proxySocket);
+    proxySocket.pipe(socket);
+  });
 });
 
 console.log(blue('Proxy server started'));
